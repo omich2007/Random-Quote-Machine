8d8862bede2e6bd82908ffb9fbf8b8f883a3d368
omich2007 <omich2007@gmail.com>
2017-08-26 12:51:51 +0300

    After validator
    Change for validador

--- Script/randomMachine.js
+++ Script/randomMachine.js
@@ -18,20 +18,3 @@ function randomQuotes() {
     var theElement = document.getElementById("message");
     theElement.innerHTML = quotes [rand] ;
 }
-
-//случайный фон для body
-
-
-// var btnStyle = document.querySelector('.btn');
-// btnStyle.addEventListener('click', randomFont)
-
- // function randomFont() {
-
-     // var backgrounds =
-     //     ["https://st.depositphotos.com/1018782/4969/v/950/depositphotos_49698733-stock-illustration-ukrainian-ornament.jpg",
-     //         "http://zoohall.com.ua/uploads/posts/2012-06/1340026527_ukrainskii-ornament-6.jpg",
-     //         "http://mtdata.ru/u5/photoC1EB/20306498144-0/original.png"]
-
-     // var randFonts = Math.floor(Math.random() * backgrounds.length);
-     // document.body.style.backgroundImage.url = "backgrounds[randFonts]";
- // }
